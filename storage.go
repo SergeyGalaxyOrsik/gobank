6f4f1eba0f6c44eea4b61ead302c0e73a445f1d9
SergeyGalaxyOrsik <sergey.orsik.work@gmail.com>
2023-12-25 17:43:51 +0300

    firstCommit

--- storage.go
+++ storage.go
@@ -1,8 +1,18 @@
 package main
 
+import "database/sql"
+
 type Storage interface {
 	CreateAccount(*Account) error
 	DeleteAccount(int) error
 	UpdateAccount(*Account) error
 	GetAccountByID(int) (*Account, error)
+}
+
+type PostgreStore struct {
+	db *sql.DB
+}
+
+func NewPostgreStore() (*PostgreStore, error) {
+
 }
